b511e7bf74aea8dd5381060196734842eea8adeb
Rahul Goswami <65107372+RahulGoswami251@users.noreply.github.com>
2020-09-25 17:12:44 +0530

    Add files via upload

---
+++ Boosting Method.R
@@ -0,0 +1,20 @@
+
+
+#Boosting
+#Data partition for model building and testing
+data(iris)
+library(C50)
+library(caret)
+set.seed(102)
+inTraininglocal<-createDataPartition(iris$Species,p=.70,list = F)
+training<-iris[inTraininglocal,]
+testing<-iris[-inTraininglocal,]
+#Model Building
+model<-C5.0(training$Species~.,data = training,trials=10) #Trials- Boosting parameter
+#Generate the model summary
+summary(model)
+#Predict for test data set
+pred<-predict.C5.0(model,testing[,-5])
+a<-table(testing$Species,pred)
+sum(diag(a))/sum(a)
+
