df288d090800189e50c5bdeecc1bc2b189b764bc
Rahul Goswami <65107372+RahulGoswami251@users.noreply.github.com>
2020-09-25 17:28:13 +0530

    Add files via upload

---
+++ Random Forest_For prediction species.R
@@ -0,0 +1,21 @@
+
+## Prepare Random forest model for iris data set #####   
+
+#Set the environment 
+install.packages("caret", dependencies = TRUE)
+install.packages("randomForest")
+library(randomForest)
+library(caret)
+
+#Load data iris set
+data("iris")
+#Prepare Radom forest model 
+model<-randomForest(iris$Species~.,data=iris,ntree=500)
+# View the forest results.
+print(model)
+#Imoporantce of the variable - Lower Gini
+print(importance(model))
+#Prediction
+pred<- predict(model,iris[,-5])
+table(pred,iris$Species)
+
